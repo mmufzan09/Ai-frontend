570590ffb21abab9e67dd0e8bdbf6492e74336c6
MuhammadMufzan <muhammadmufzan787@gmail.com>
2025-09-07 23:40:32 +0500

    changes

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const UserDataContext = createContext();
 
 function UserContext({ children }) {
-  const serverUrl = "https://ai-backend-production-cf44.up.railway.app";
+  const serverUrl = "https://ai-backend-production-6448.up.railway.app";
 
   // ✅ ek hi axios instance
   const api = axios.create({
--- src/pages/customize2.jsx
+++ src/pages/customize2.jsx
@@ -11,7 +11,7 @@ function Customize2() {
 
  const serverUrl =
   import.meta.env.VITE_SERVER_URL ||
-  "https://ai-backend-production-cf44.up.railway.app";
+  "https://ai-backend-production-6448.up.railway.app";
 
   const handleUpdateAssistant = async () => {
     try {
